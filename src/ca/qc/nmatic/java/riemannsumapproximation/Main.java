1b4e9c9c0e70d281f88e681fb451dbe7dc0a3a11
Victor B <vicbab@yandex.com>
2017-04-05 18:46:24 -0400

    Initial Commit

---
+++ src/ca/qc/nmatic/java/riemannsumapproximation/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Author: Victor Babin (vicbab)
+ * Project: RiemannSumApproximation
+ * Description: A tool to get an approximation of a Riemann sum.
+ * Version: v1.0 beta
+ */
+package ca.qc.nmatic.java.riemannsumapproximation;
+
+/**
+ *
+ * @author Victor Babin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
