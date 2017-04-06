9127bbd24410588ddd58f1e13510c27b4e855a0e
Victor B <vicbab@yandex.com>
2017-04-05 22:15:59 -0400

    it's better now

--- src/ca/qc/nmatic/java/riemannsumapproximation/Main.java
+++ src/ca/qc/nmatic/java/riemannsumapproximation/Main.java
@@ -7,6 +7,7 @@
 package ca.qc.nmatic.java.riemannsumapproximation;
 
 import java.lang.Math;
+import java.util.Scanner;
 
 /**
  *
@@ -16,31 +17,42 @@ public class Main {
 
     public static final int K = 0;
     public static final double N = Math.pow(2, 17);
-    // public static final int N = 15;
+    // funcX(a + delX * i, 1, 10, 30)
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double[] funcX = new double[8];
-        double sum;
-        sum = getSum(0, 10, funcX);
-        System.out.println("Area under the curve: " + sum);
+        double sum, borneSup, borneInf;
+        boolean restart = true;
+        Scanner sc = new Scanner(System.in);
+        while(restart){
+            try {
+                System.out.print("Borne supérieure: ");
+                borneSup = Integer.parseInt(sc.nextLine());
+                System.out.print("Borne inférieure: ");
+                borneInf = Integer.parseInt(sc.nextLine());
+                sum = getSum(borneInf, borneSup);
+                System.out.println("Area under the curve: " + sum);
+                restart = false;
+            } catch (NumberFormatException e) {
+                restart = true;
+            }    
+        }        
     }
 
     /**
      * @param a borne inférieure
      * @param b borne supérieure
-     * @param funcX the function in question
      * @return result for a sum
      */
-    public static double getSum(int a, int b, double[] funcX) {
+    public static double getSum(double a, double b) {
         if (b > a) {
             double result = 0;
             int numberOfIterations = K;
             double delX = (b - a) / N;
             for (int i = K; i < N; i++) {
-                result += (funcX(a + delX * i, 1) * delX);
+                result += (funcX(a + delX * i, 1, 0, 1) * delX);
                 numberOfIterations++;
             }
             System.out.println("Number of iterations: " + numberOfIterations);
@@ -50,7 +62,7 @@ public class Main {
         }
     }
 
-    public static double funcX(double x, double puissance) {
-        return Math.pow(x, puissance);
+    public static double funcX(double x, double puissance, double constante, double coefficient) {
+        return (coefficient * Math.pow(x, puissance) + constante);
     }
 }
